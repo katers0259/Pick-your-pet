dc4f9e8802cdf2709709ced54e5e13c34d34c012
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2020-10-28 22:01:25 -0600

    adding more functionality with populating to the dom with cards

--- script.js
+++ script.js
@@ -34,28 +34,61 @@ $(document).ready(function () {
       }
     }).then(function (response) {
       // do something with results
+      var speciesSelection;
+      var sizeSelection;
+      var genderSelection;
+      $("#submit").on("click", function (e) {
+        e.preventDefault();
 
-      for (let i = 0; i < response.animals.length; i++) {
-        console.log(response.animals[i]);
-        console.log(i);
+        //console.log(response.animals[0].species[0])
 
-        $("#submit").on("click", function (e) {
-          e.preventDefault();
-          var species = response.animals[i].species[0];
-          var cat = $(".species-1")
-          var dog = $(".species-0")
-          console.log(species[0])
-          if (species === cat) {
-            $(`species${0}`).appendTo($(".species"))
+        speciesSelection = $("#petType").val()
+        sizeSelection = $("#petSize").val()
+        genderSelection = $("#petGender").val()
+        console.log(speciesSelection)
+        console.log(sizeSelection)
+        console.log(genderSelection)
+        for (var i = 0; i < response.animals.length; i++) {
+          console.log(response.animals[i]);
+          //console.log(i);
+          var gender = response.animals[i].gender;
+          var species = response.animals[i].species;
+          var weight = response.animals[i].size;
+          var age = response.animals[i].age;
+          var breed = response.animals[i].breeds.primary;
+         var name = response.animals[i].name;
+         var contact = response.animals[i].contact.email
+         var picture = response.animals[i].photos[0]
+          if (species === speciesSelection && sizeSelection && genderSelection) {
+            var container = $("<div>").addClass("card").html(`<div class="name" class="clear"></div>
+            <img class="picture">${picture}</img>
+            <div class="name" class="clear">${name}</div>
+            <div class="age" class="clear">${age}</div>
+            <div class="breed" class="clear">${breed}</div>
+            <div class="gender" class="clear">${gender}</div>
+            <div class="size" class="clear">${weight}</div>
+            <div class="species" class="clear">${species}</div>
+            <div class="contact" class="clear">${contact}</div>`)
             
-          };
-
-
-
+            $("#petDisplay").append(container);
             $(".clear").empty()
-          
-        });
-      };
+
+          };
+        };
+
+
+        //console.log(species.cat)
+
+
+
+
+
+
+       
+
+      });
+
+
 
     });
   };
